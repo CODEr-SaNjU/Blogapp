485a89222830f712e5f5961ccae3a02f999d1fb6
~Coder_SaNjU <sanju2help@gmail.com>
2024-07-17 16:28:20 +0530

    add test case for business loginc

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -1,21 +1,84 @@
-import requests
+# blog_app/tests/test_views.py
 
-# Obtain token
-response = requests.post('http://127.0.0.1:8000/api/token/', data={
-    'username': 'coder_sanju',
-    'password': 'sanjummp143@'
-})
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Post, Comment
+from rest_framework_simplejwt.tokens import RefreshToken
 
-token = response.json()['access']
+class PostAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='Test Content',
+            author=self.user
+        )
+        self.url = reverse('post-list-create')
+        self.detail_url = reverse('post-detail', kwargs={'pk': self.post.pk})
+        self.token = RefreshToken.for_user(self.user).access_token
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
 
-headers = {
-    'Authorization': f'Bearer {token}'
-}
-print(headers)
-data = {
-    'title': 'New Post',
-    'content': 'This is the content of the new post.',
-    'author': 1  # Assuming author ID is 1
-}
-response = requests.post('http://127.0.0.1:8000/api/posts/', headers=headers, data=data)
-print(response.json())
+    def test_get_posts(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_create_post(self):
+        data = {
+            'title': 'New Post',
+            'content': 'New Content',
+            'author': '1'
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count(), 2)
+
+    def test_update_post(self):
+        data = {
+            'title': 'Updated Post',
+            'content': 'Updated Content',
+            'author': '1'
+        }
+        response = self.client.put(self.detail_url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.post.refresh_from_db()
+        self.assertEqual(self.post.title, 'Updated Post')
+
+    def test_delete_post(self):
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Post.objects.count(), 0)
+
+class CommentAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.post = Post.objects.create(
+            title='Test Post',
+            content='Test Content',
+            author=self.user
+        )
+        self.comment = Comment.objects.create(
+            post=self.post,
+            author=self.user,
+            text='Test Comment'
+        )
+        self.url = reverse('comment-list-create', kwargs={'post_id': self.post.pk})
+        self.token = RefreshToken.for_user(self.user).access_token
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_get_comments(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], 1)
+
+    def test_create_comment(self):
+        data = {
+            'post': '1',
+            'text': 'New Comment'
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Comment.objects.count(), 2)
+
